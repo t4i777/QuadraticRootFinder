175f41cda039cdbbef974d6505d401f1ba472119
Taii777 <tylerjbarnes0726@gmail.com>
2025-12-24 18:17:29 -0500

    my first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project No. 1
+
+This is my first GitHub project. I published this project directly from Visual Studio Code.
+
+This project is a quadratic root finder that calculates real or complex roots of a quadratic. I used HTML, JavaScript, and CSS.
+
+Dec. 24, 2025
+Tyler J. Lowery
---
+++ index.js
@@ -0,0 +1,56 @@
+document.getElementById("mySubmit").onclick = function() {
+    const aInput = document.getElementById("aValue").value.trim();
+    const bInput = document.getElementById("bValue").value.trim();
+    const cInput = document.getElementById("cValue").value.trim();
+    const digits = 5;
+
+    if (aInput == "" || bInput == "" || cInput == "") {
+        document.getElementById("x1").textContent = "Please enter all fields.";
+        document.getElementById("x2").textContent = "";
+        return;
+    }
+
+    const a = Number(aInput);
+    const b = Number(bInput);
+    const c = Number(cInput);
+
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        document.getElementById("x1").textContent = "Please enter valid numbers!";
+        document.getElementById("x2").textContent = "";
+        return;
+    }
+
+    if (a == 0) {
+        document.getElementById("x1").textContent = "Not a quadratic!";
+        return;
+    }
+    
+    const discriminant = b**2 - 4*a*c;
+
+    if (discriminant < 0) {
+        let realPart = Number(-b / (2*a)).toFixed(digits);
+        realPart = parseFloat(realPart);
+        let complexPart = Number(Math.sqrt(Math.abs(discriminant)) / (2 * a)).toFixed(digits);
+        complexPart = parseFloat(complexPart);
+        document.getElementById("x1").innerHTML = `x<sub>1</sub> = ${realPart} - ${complexPart}i`;
+        document.getElementById("x2").innerHTML = `x<sub>2</sub> = ${realPart} + ${complexPart}i`;
+        return;
+    }
+
+    if (discriminant == 0) {
+        let x = ((-b + Math.sqrt(discriminant)) / (2 * a)).toFixed(digits);
+        x = parseFloat(x);
+        document.getElementById("x1").textContent = `x = ${x}`;
+        document.getElementById("x2").textContent = "";
+        return;
+    }
+
+    let x1 = ((-b - Math.sqrt(discriminant)) / (2 * a)).toFixed(digits);
+    x1 = parseFloat(x1);
+
+    let x2 = ((-b + Math.sqrt(discriminant)) / (2 * a)).toFixed(digits);
+    x2 = parseFloat(x2);
+
+    document.getElementById("x1").innerHTML = `x<sub>1</sub> = ${x1}`;
+    document.getElementById("x2").innerHTML = `x<sub>2</sub> = ${x2}`;
+}
